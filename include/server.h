87c195d62cb4a91e992af526fa31b46c63cab9d7
Ankush <mondalankush9851@gmail.com>
2023-12-20 15:31:15 +0530

    Defined the compiler and the OS prepocessor Directives

---
+++ include/server.h
@@ -0,0 +1,30 @@
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include "webflow.h"
+
+//Base Class
+class Server {
+	private:
+		int socketfd;
+
+	public:
+		//
+	
+	protected:
+		//
+};
+
+//Derived Class
+class HttpServer {
+	private:
+		//
+	
+	public: 
+		//
+	
+	protected:
+		//
+};
+
+#endif //SERVER_H_
