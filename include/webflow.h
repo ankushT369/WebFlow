a963c0347ec48d1d0aebe83e67614f5312d339fd
Ankush <mondalankush9851@gmail.com>
2023-12-21 20:19:50 +0530

    eliminated the tab

--- include/webflow.h
+++ include/webflow.h
@@ -1,35 +1,32 @@
 #pragma once
 
 #ifdef __GNUC__
-	#if (__GNUC__ > 6) || (__GNUC__ == 6 && __GNUC_MINOR__ >= 2)
-		#include <vector>	
-		#include <cstring>
-		//Other Headers
-	#else	
-		#error This code requries GCC version 6.2.0 or newer
-	#endif
+#if (__GNUC__ > 6) || (__GNUC__ == 6 && __GNUC_MINOR__ >= 2)
+	#include <vector>	
+	#include <cstring>
+	//Other Headers
+#else	
+#error This code requries GCC version 6.2.0 or newer
+#endif
 #else 
-	#error This code requires the GCC compiler
+#error This code requires the GCC compiler
 #endif
 
 #if defined(__linux__)
-	// Headers of POSIX Socket API
+// Headers of POSIX Socket API
 
-	#include <cstdio>
-	#include <cstdlib>
-	#include <error.h>
-	#include <unistd.h>
-	#include <sys/types.h>
-	#include <sys/socket.h>
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
-	#include <sys/wait.h>
-	#include <netdb.h>
-
-
+#include <cstdio>
+#include <cstdlib>
+#include <error.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <netdb.h>
 #else
-	#warning _LINUX_ is not defined
-	#error This code intended for LINUX environment
+#error This code intended for LINUX environment
 #endif
 
 
