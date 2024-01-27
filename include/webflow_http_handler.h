690c833e4f9b9bf3511229163708ff2161deb960
ankush1233 <ankushmondal1y2t@gmail.com>
2024-01-27 19:07:32 +0530

    rectified the compilation errors

--- include/webflow_http_handler.h
+++ include/webflow_http_handler.h
@@ -7,20 +7,14 @@
 #include <functional>
 
 namespace webflow {
-/*
- *
- *
- *
- *
- *
- *
- */
+
 //Http method map
-#define HTTP_METHOD_MAP(XX)		\
-	XX(0, DELETE, DELETE)		\
-	XX(1, GET, GET)				\
-	XX(2, POST, POST)			\
-	XX(3, PUT, PUT)				\
+#define HTTP_METHOD_MAP(XX)								\
+	XX(1, DELETE, DELETE)								\
+	XX(2, GET, GET)										\
+	XX(3, POST, POST)									\
+	XX(4, PUT, PUT)										\
+	XX(0, METHOD_ERROR, METHOD_ERROR)									\
 
 enum class HttpMethod {
 #define XX(num, name, string) HTTP_##name = num,
@@ -29,30 +23,23 @@ enum class HttpMethod {
 };
 
 //http verison map
-#define HTTP_VERSION(XX)			\
-	XX(11, _1_1)				\
-
+#define HTTP_VERSION(XX)								\
+	XX(11, _1_1, HTTP/1.1)								\
+	XX(-1, _VERSION_NOT_FOUND, NOT_FOUND)								
 
 enum class HttpVersion {
-#define XX(num , version) HTTP##version = num,
+#define XX(num, version, string) HTTP##version = num,
 	HTTP_VERSION(XX)
 #undef XX
 };
 
 #define CR '\r'
 #define NL '\n'
-#define DNL "\n\n"
 #define SP ' '
 /*
  *
  *
- *
- *
- *
- *
- *
  */
-
 struct HttpRequest {
 	HttpMethod method_;
 	std::string uri;
@@ -66,14 +53,15 @@ public:
 	HttpMessage() = default;
 	explicit HttpMessage(std::string http_message, int sock);
 	virtual ~HttpMessage() = default;
+	HttpMethod GetHttpMethod(const std::string&, HttpRequest&);
 
 protected:
 	std::string http_message_;
 	int sock_;
 	void ParseHttpRequest(const std::string&);
+	std::string GetHttpUri(const std::string&);
+	HttpVersion GetHttpVersion(const std::string&, HttpRequest&);
 };
-
-
 
 }
 #endif // WEBFLOW_HTTP_HANDLER_H
--- src/webflow_http_handler.cc
+++ src/webflow_http_handler.cc
@@ -12,38 +12,70 @@ HttpMessage::HttpMessage(std::string msg, int socket):
 	}
 
 void HttpMessage::ParseHttpRequest(const std::string& request) {
+
+	std::string line;
+	std::string http_method;
+	std::string http_uri;
+	std::string http_version;
+
 	std::istringstream iss(request);
-    std::string line;
-
-    // Parse the request line
-    std::getline(iss, line);
-    std::istringstream requestLineStream(line);
-    HttpRequest httpRequest;
+	std::getline(iss, line, NL);
+	std::istringstream requestLineStream(line);	
 	
-	fmt::print("{}\n", line);
-    //requestLineStream >> httpRequest.method_ >> httpRequest.uri >> httpRequest.version_;
+	std::getline(requestLineStream, http_method, SP);
+	std::getline(requestLineStream, http_uri, SP);
+	std::getline(requestLineStream, http_version, SP);
 
-    // Parse headers
-    while (std::getline(iss, line) && !line.empty()) {
-        size_t colonPos = line.find(':');
-        if (colonPos != std::string::npos) {
-            std::string headerName = line.substr(0, colonPos);
+    HttpRequest httpRequest;
+
+	httpRequest.method_ = GetHttpMethod(http_method, httpRequest);
+	httpRequest.uri = http_uri;
+	httpRequest.version_ = GetHttpVersion(http_version, httpRequest);
+	
+	while (std::getline(iss, line) && !line.empty()) { 
+		size_t colonPos = line.find(':');
+
+        if (colonPos != std::string::npos) {	
+			std::string headerName = line.substr(0, colonPos);
             std::string headerValue = line.substr(colonPos + 2);  // Skip ': ' after the header name
             httpRequest.header_[headerName] = headerValue;
         }
     }
-	for(const auto& elem : httpRequest.header_) {
-		std::cout << elem.first << " " << elem.second << "\n";
-	}
-    // Display parsed information
-   /* std::cout << "Method: " << httpRequest.method << std::endl;
-    std::cout << "Path: " << httpRequest.path << std::endl;
-    std::cout << "HTTP Version: " << httpRequest.version << std::endl;
-	*/
+
 	return ;
 }
 
+HttpMethod HttpMessage::GetHttpMethod(const std::string& method, HttpRequest& httprequest) {
+	//print method
+	/*switch(method) {
+		#define XX(num, name, string) case HttpMethod::HTTP_##name: std::cout << "HTTP " #name << std::endl; break;
+			HTTP_METHOD_MAP(XX)
+		#undef XX
+		default:
+			std::cout << "Invalid HTTP method" << std::endl;
+        	break;
+	}*/
+	if (method == "DELETE") {
+        return HttpMethod::HTTP_DELETE;
+    } else if (method == "GET") {
+        return HttpMethod::HTTP_GET;
+    } else if (method == "POST") {
+        return HttpMethod::HTTP_POST;
+    } else if (method == "PUT") {
+        return HttpMethod::HTTP_PUT;
+    } else {
+        return HttpMethod::HTTP_METHOD_ERROR;
+    }
+	//return ;
 
+}
 
+HttpVersion HttpMessage::GetHttpVersion(const std::string& version, HttpRequest& httprequest) {
+	if(version == "HTTP/1.1") {
+		return HttpVersion::HTTP_1_1;
+	} else {
+		return HttpVersion::HTTP_VERSION_NOT_FOUND;
+	}
+}
 
 }//end of namespace
